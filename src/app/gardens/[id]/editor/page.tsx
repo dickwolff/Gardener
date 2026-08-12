ed86005d4c56e84a01780059f1805fa675724c03
Dick Wolff <dckwlff@gmail.com>
2026-08-12 11:10:52 +0200

    Fix editor pagina layout zodat deze niet groter is dan viewport

--- src/app/gardens/[id]/editor/page.tsx
+++ src/app/gardens/[id]/editor/page.tsx
@@ -28,7 +28,7 @@ export default async function GardenEditorPage({ params }: GardenEditorPageProps
   }
 
   return (
-    <>
+    <div className="h-dvh flex flex-col overflow-hidden">
       <Header />
       <main className="flex-1 flex flex-col mx-auto max-w-[1280px] w-full px-12 py-8 min-h-0">
         <div className="mb-4">
@@ -64,6 +64,6 @@ export default async function GardenEditorPage({ params }: GardenEditorPageProps
           <GardenEditor garden={garden} />
         </div>
       </main>
-    </>
+    </div>
   );
 }
